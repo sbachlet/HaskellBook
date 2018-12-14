659d97618ba2577cda4ab5be68770d71c1888d52
Shawn_Work <bachlet-shawn@n-ask.com>
2018-12-14 16:02:29 -0700

    feat :: Update to chapter 9

--- chapter9/enumFromTo.hs
+++ chapter9/enumFromTo.hs
@@ -2,23 +2,23 @@ module EnumFromTo where
 eftBool :: Bool -> Bool -> [Bool]
 eftBool x y
   | x > y       = []
-  | x == y      = [y]
-  | otherwise   = [x] ++ eftBool (succ x) y
+  | x == y      = (:[]) y
+  | otherwise   = (:) x $ eftBool (succ x) y
 
 eftOrd :: Ordering -> Ordering -> [Ordering]
 eftOrd x y
   | x > y       = []
-  | x == y      = [y]
-  | otherwise   = [x] ++ eftOrd (succ x) y
+  | x == y      = (:[]) y
+  | otherwise   = (:) x $ eftOrd (succ x) y
 
 eftInt :: Int -> Int -> [Int]
 eftInt x y
   | x > y       = []
-  | x == y      = [y]
-  | otherwise   = [x] ++ eftInt (succ x) y
+  | x == y      = (:[]) y
+  | otherwise   = (:) x $ eftInt (succ x) y
 
 eftChar :: Char -> Char -> [Char]
 eftChar x y
   | x > y       = []
-  | x == y      = [y]
-  | otherwise   = [x] ++ eftChar (succ x) y
+  | x == y      = (:[]) y
+  | otherwise   = (:) x $ eftChar (succ x) y
