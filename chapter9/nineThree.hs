efa37949a4e1de0d34aade4e60ab2b567d79b275
Shawn_Work <bachlet-shawn@n-ask.com>
2018-12-14 15:55:44 -0700

    feat :: Finished 9.5

---
+++ chapter9/nineThree.hs
@@ -0,0 +1,13 @@
+module NineThree where
+myTail :: [a] -> [a]
+myTail [] = []
+myTail (_ : xs) = xs
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (x:[]) = Nothing
+safeTail (_:xs) = Just xs
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
