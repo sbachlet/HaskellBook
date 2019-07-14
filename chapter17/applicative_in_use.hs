9f43ce669ca040cce8324ecf3b66eed4660788bc
sbachlet <sbachlet@gmail.com>
2019-07-14 17:19:54 -0600

    work done on applicative in use exercises

--- chapter17/applicative_in_use.hs
+++ chapter17/applicative_in_use.hs
@@ -1,4 +1,5 @@
 import Control.Applicative
+import Data.List (elemIndex)
 
 f :: Int -> Maybe String
 f x = lookup x [ (3, "hello")
@@ -16,6 +17,18 @@ h z = lookup z [(2,3), (5,6), (7,8)]
 m :: Int -> Maybe Int
 m x = lookup x [(4,10), (8,13), (1,9001)]
 
+added :: Maybe Integer
+added = (+3) <$> (lookup 3 $ zip [1,2,3] [4,5,6])
+
+y :: Maybe Integer 
+y = lookup 3 $ zip [1,2,3] [4,5,6]
+
+z :: Maybe Integer 
+z = lookup 2 $ zip [1,2,3] [4,5,6]
+
+tupled :: Maybe (Integer, Integer)
+tupled = (,) <$> y <*> z
+
 main :: IO ()
 main = do 
   print $ f 3
@@ -27,8 +40,10 @@ main = do
   print $ liftA2 (^) (h 5) (m 4)
   print $ liftA2 (*) (h 5) (m 4)
   print $ liftA2 (*) (h 1) (m 1)
-  a <- fmap length $ liftA2 (++) getLine getLine
-  b <- liftA2 (,) getLine getLine
-  print $ a
-  print $ b
+  -- a <- fmap length $ liftA2 (++) getLine getLine
+  -- b <- liftA2 (,) getLine getLine
+  -- print $ a
+  -- print $ b
+  print added
+  print tupled
 
